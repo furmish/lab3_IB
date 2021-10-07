0288e84bde327f79ab31417f066adfec83851f23
trilliant <mrzeero0@mail.ru>
2021-10-08 01:36:21 +0300

    Added common @secretFunc(int key, int value) into the interface

--- src/FeistelCipher.java
+++ src/FeistelCipher.java
@@ -1,5 +1,8 @@
 public interface FeistelCipher {
-    int[] RESHUFFLE = new int[]{4,1,3,2};
+    int[] RESHUFFLE = new int[]{4, 1, 3, 2};
     int[] KEYS = new int[]{354453, 256453, 191146};
     int ALPHABET_SIZE = 65536;
+    default int secretFunc(int key, int value) {
+        return (key * value) % ALPHABET_SIZE;
+    }
 }
