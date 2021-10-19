8febc4259624bcd6667db29e85f8d43ab717feb0
triIIiant <mrzeero0@mail.ru>
2021-10-19 18:08:15 +0300

    changed interface to abstract class

--- src/FeistelCipher.java
+++ src/FeistelCipher.java
@@ -1,5 +1,5 @@
 /**
- * Интерфейс шифра Фейстеля.<p>
+ * Абстрактный класс шифра Фейстеля.<p>
  * Содержит константы:<p>
  * <blockquote><pre>
  * {@link #KEYS} - массив ключей
@@ -7,17 +7,19 @@
  * {@link #ALPHABET_SIZE} - размер алфавита </blockquote></pre><p>
  * И общую функцию для шифрования и дешифрования {@link #secretFunc(int, int)}
  */
-public interface FeistelCipher {
-    int[] RESHUFFLE = new int[]{4,1,3,2};
-    int[] KEYS = new int[]{354453, 256453, 191146};
-    int ALPHABET_SIZE = 65536;
+public abstract class FeistelCipher {
+    public static final int[] RESHUFFLE = new int[]{2, 1, 4, 3};
+    public static final int[] KEYS = new int[]{456, 123, 1053};
+    public static final int ALPHABET_SIZE = 65536;
+
     /**
      * Функция, использующаяся для шифрования
-     * @param key ключ
+     *
+     * @param key   ключ
      * @param value значение
      * @return измененное значение
      */
-    default int secretFunc(int key, int value) {
+    public int secretFunc(int key, int value) {
         return (key * value) % ALPHABET_SIZE;
     }
 }
